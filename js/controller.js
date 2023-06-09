acd9dd752ac98f5bdce51802d5f5d63d61892d23
KamilRecast <100204993+KamilRecast@users.noreply.github.com>
2023-06-09 13:43:56 +0100

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -64,8 +64,7 @@ const controlBookmark = () => {
   bookmarksView.closeModals();
 };
 const init = async function () {
-  const screenWidthElement = document.getElementById('screen-width');
-screenWidthElement.textContent = `Szerokość ekranu: ${window.innerWidth}px`;
+
   await model.getPosition();
   console.log("START");
 
